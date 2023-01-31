44543873e3a9ca21668e712f395ce091405eb489
denad00 <danika.enad@gmail.com>
2023-01-30 16:18:07 -0800

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, TextInput, View} from 'react-native'
+
+
+const Form = () => {
+    <View>
+        <Text>Open Weather App</Text>
+        <TextInput />
+    </View>
+}
+
+export default Form
